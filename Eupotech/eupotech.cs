50f7ff01ba4e083ea654823da5ee432c47f0201f
Timmo2022 <timmomutumwa1@gmail.com>
2022-03-26 11:17:30 +0300

    Eupotech

---
+++ Eupotech/eupotech.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Eupotech
+{
+  class Interview
+  {
+    static void Main(string[] args)
+    {
+      string[] place = {"BandaLane", "Karen", "Nairobi", "Kenya"};
+      Console.WriteLine(place[0]);
+    }
+  }
+}
